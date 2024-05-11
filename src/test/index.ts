e87a2a75cf6039d83a7264fafeae2248e44b2eb9
Andrew Nolte <anolte512@gmail.com>
2024-05-11 19:38:22 -0400

    ci changes

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,6 +1,6 @@
 import * as path from 'path'
-import * as Mocha from 'mocha'
-import * as glob from 'glob'
+import glob from 'glob'
+import Mocha from 'mocha'
 
 export function run(): Promise<void> {
   // Create the mocha test
@@ -11,7 +11,7 @@ export function run(): Promise<void> {
   const testsRoot = path.resolve(__dirname, '..')
 
   return new Promise((c, e) => {
-    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+    glob('**/**.test.js', { cwd: testsRoot }, (err: Error | null, files: string[]) => {
       if (err) {
         return e(err)
       }
